b8d6fa7a75e8622156ac710976eac2186db4de2f
emmpak <eiliev05@yahoo.com>
2021-05-14 15:08:08 +0100

    Add base and index template

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-  return "Computer says no."
+  user = { 'username': 'Emil' }
+  return render_template('index.html', user=user)
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -10,6 +10,7 @@ class TestRoutes(unittest.TestCase):
   def test_index_page(self):
     res = self.client.get('/')
     self.assertEqual(res.status_code, 200)
+    self.assertIn(b'Welcome back, Emil', res.data)
 
 if __name__ == '__main__':
     unittest.main()
