3ab1883beb9141690010bd313bfeb64663a355a0
emmpak <eiliev05@yahoo.com>
2021-05-15 13:45:14 +0100

    Add sessions and current_user

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,33 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, session, g
 from app import app
 from app.models import User, Transfer
 from app.forms import LoginForm
+
+@app.before_request
+def before_request():
+  g.current_user = None
+  if 'user_id' in session:
+    g.current_user = User.query.get(int(session['user_id']))
 
 @app.route('/')
 @app.route('/index')
 @app.route('/transfers')
 def index():
-  user = User(username='user')
-  transfer = Transfer(currency="USD", amount=100, originator=user)
-  return render_template('index.html', user=user)
+  if not g.current_user:
+    return redirect(url_for('login'))
+
+  return render_template('index.html')
 
 @app.route('/login', methods=['post', 'get'])
 def login():
+  if g.current_user:
+    return redirect(url_for('index'))
   form = LoginForm()
   if form.validate_on_submit():
+    user = User.query.filter_by(username=form.username.data).first()
+    if user is None or not user.check_password(form.password.data):
+      return redirect(url_for('login'))
+    session['user_id'] = user.id
     return redirect(url_for('index'))
   return render_template('login.html', form=form)
 
